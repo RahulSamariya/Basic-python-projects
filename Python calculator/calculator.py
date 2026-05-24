357ad9d8fb5540d6890cded2a6ed09b5d52cabbc
Shrey Shah <rahulsamariya008@gmail.com>
2026-05-25 00:22:23 +0530

    Update calculator.py
    perfect window grid is completed

--- Python calculator/calculator.py
+++ Python calculator/calculator.py
@@ -2,9 +2,9 @@ import tkinter as tk
 
 button_values = [
     ['AC', '+/-', '%', '÷', '∑'],
-    ['9', '8', '7', 'x', 'π'],
-    ['6', '5', '4', '-', '^'],
-    ['3', '2', '1', '+', '//'],
+    ['7', '8', '9', 'x', 'π'],
+    ['4', '5', '6', '-', '^'],
+    ['1', '2', '3', '+', '//'],
     ['0', '.', '√', '=', '!']
 ]
 
@@ -28,18 +28,18 @@ window.title('Calculator')
 window.resizable(False, False)
 
 frame = tk.Frame(window, bg=colour_gray_blue)
-label = tk.Label(frame, text='0', font=('Arial', 45), bg=colour_warm_blush_pink,
-                fg=colour_lake)
-                
-label.grid(row=0, column=0, columnspan=column_count, rowspan=1, padx=5, pady=5, sticky='nsew')
+label = tk.Label(frame, text='0', font=('Arial', 45), bg=colour_lake, 
+                fg=colour_lake, anchor='e')
+            
+label.grid(row=0, column=0, columnspan=column_count, rowspan=1, padx=8, pady=8, sticky='nsew')
 
 for row in range(row_count):
     for col in range(column_count): 
         value = button_values[row][col]
         button = tk.Button(frame, text=value, font=('Arial', 15),
-                           width=5, height=2, bg=colour_soft_stone_gray,
+                           width=5, height=2, bg=colour_soft_stone_gray, fg=colour_lake,
                            command=lambda value=value: button_clicked(value))
-        button.grid(row=row, column=col, padx=5, pady=5  )
+        button.grid(row=row+1, column=col, padx=5, pady=5,)
 
 def button_clicked(value):
     pass
